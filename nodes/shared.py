916a7e9a93033ca6d7359c55804150884df2d1c6
Marco João <marco.joao@signature.ai>
2024-08-16 14:11:59 +0100

    create fallback

--- nodes/shared.py
+++ nodes/shared.py
@@ -1,15 +1,15 @@
 import os
 
-from comfy import model_management  # type: ignore
 from dotenv import load_dotenv
+from signature_core import CURRENT_DIR
 
 load_dotenv()
 
-COMFY_DEVICE = model_management.get_torch_device()
 BASE_COMFY_DIR = os.path.dirname(os.path.realpath(__file__)).split("custom_nodes")[0]
-SIGNATURE_DIR = os.path.dirname(os.path.realpath(__file__)).split("src")[0]
-SD_SCRIPTS_DIR = os.getenv("SD_SCRIPTS_DIR")
-LORA_OUTPUT_DIR = os.getenv("LORA_OUTPUT_DIR")
+SIGNATURE_NODES_DIR = os.path.dirname(os.path.realpath(__file__)).split("src")[0]
+SIGNATURE_CORE_DIR = CURRENT_DIR
+SD_SCRIPTS_DIR = os.getenv("SD_SCRIPTS_DIR") or os.path.join(CURRENT_DIR, "submodules/sd-scripts")
+LORA_OUTPUT_DIR = os.getenv("LORA_OUTPUT_DIR") or os.path.join(BASE_COMFY_DIR, "models/loras")
 
 
 class AnyType(str):
