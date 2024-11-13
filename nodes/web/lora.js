9b644df7ecb2c3a93f0eca55b13afc20b71c8bbd
Marco João <marco.joao@signature.ai>
2024-11-13 17:28:44 +0000

    Clean up lora.js

--- nodes/web/lora.js
+++ nodes/web/lora.js
@@ -14,7 +14,6 @@ function handleNumSlots(node, widget) {
 
     widgets.forEach((w) => {
       if (i <= numSlots) {
-        // Show widget - restore from hidden array if it exists
         const hiddenIndex = node.widgetsHidden.indexOf(w);
         const widgetIndex = node.widgets.indexOf(w);
         if (hiddenIndex !== -1 && widgetIndex === -1) {
@@ -22,7 +21,6 @@ function handleNumSlots(node, widget) {
           node.widgetsHidden.splice(hiddenIndex, 1);
         }
       } else {
-        // Hide widget - move to hidden array
         const widgetIndex = node.widgets.indexOf(w);
         if (widgetIndex !== -1) {
           node.widgetsHidden.push(w);
@@ -32,7 +30,6 @@ function handleNumSlots(node, widget) {
     });
   }
 
-  // Trigger node size recalculation
   handleMode(node, undefined);
 }
 
@@ -59,7 +56,6 @@ function handleMode(node, widget) {
           (widget) => widget.name === `lora_name_${slotNum}`,
         );
 
-        // Only proceed if corresponding lora_name exists
         if (loraNameWidget !== -1) {
           const hiddenIndex = node.widgetsHiddenModes.indexOf(w);
           const widgetIndex = node.widgets.indexOf(w);
@@ -70,7 +66,6 @@ function handleMode(node, widget) {
         }
       }
       if (isAdvancedWidget) {
-        // Hide advanced widgets
         const widgetIndex = node.widgets.indexOf(w);
         if (widgetIndex !== -1) {
           node.widgetsHiddenModes.push(w);
@@ -79,7 +74,6 @@ function handleMode(node, widget) {
       }
     } else {
       if (isWeightWidget) {
-        // Hide simple weight widgets
         const widgetIndex = node.widgets.indexOf(w);
         if (widgetIndex !== -1) {
           node.widgetsHiddenModes.push(w);
@@ -92,7 +86,6 @@ function handleMode(node, widget) {
           (widget) => widget.name === `lora_name_${slotNum}`,
         );
 
-        // Only proceed if corresponding lora_name exists
         if (loraNameWidget !== -1) {
           const hiddenIndex = node.widgetsHiddenModes.indexOf(w);
           const widgetIndex = node.widgets.indexOf(w);
@@ -105,7 +98,6 @@ function handleMode(node, widget) {
     }
   });
 
-  // Trigger node size recalculation
   node.setSize(node.computeSize());
 }
 
@@ -127,7 +119,6 @@ const ext = {
   nodeCreated(node) {
     const className = node.comfyClass;
     if (className === "signature_lora_stacker") {
-      // Setup widget value properties first
       for (const w of node.widgets || []) {
         let widgetValue = w.value;
         let originalDescriptor = Object.getOwnPropertyDescriptor(w, "value");
@@ -148,7 +139,6 @@ const ext = {
               widgetValue = newVal;
             }
 
-            // Only call widgetLogic if the value actually changed
             if (oldVal !== newVal) {
               widgetLogic(node, w);
             }
@@ -156,7 +146,6 @@ const ext = {
         });
       }
 
-      // Trigger initial setup after node is created
       const numSlotsWidget = node.widgets.find((w) => w.name === "num_slots");
       const modeWidget = node.widgets.find((w) => w.name === "mode");
 
@@ -171,78 +160,3 @@ const ext = {
 };
 
 app.registerExtension(ext);
-
-// app.registerExtension({
-//     name: "signature.lora_stacker",
-//     async beforeRegisterNodeDef(nodeType, nodeData, app) {
-//         const class_name = nodeType.comfyClass;
-//         if (class_name === "signature_lora_stacker") {
-//             // Override the onConnectionsChange method to update visibility
-//             nodeType.prototype.onPropertyChanged = function (name, value) {
-//                 const oldValue = this[name];
-//                 if (oldValue === value) return; // Skip if value hasn't changed
-
-//                 this[name] = value; // Update the value
-
-//                 // Handle num_slots changes
-//                 if (name === "num_slots") {
-//                     const numSlots = parseInt(value);
-//                     // Remove or restore inputs based on selected number of slots
-//                     for (let i = 1; i <= 10; i++) {
-//                         const inputs = this.inputs.filter(input =>
-//                             input.name.endsWith(`_${i}`)
-//                         );
-
-//                         if (i > numSlots) {
-//                             // Remove inputs for unused slots
-//                             inputs.forEach(input => {
-//                                 const index = this.inputs.indexOf(input);
-//                                 if (index > -1) {
-//                                     this.inputs.splice(index, 1);
-//                                 }
-//                             });
-//                         } else {
-//                             // Restore inputs if they don't exist
-//                             const hasLora = this.inputs.some(input => input.name === `lora_${i}`);
-//                             if (!hasLora) {
-//                                 this.inputs.push(
-//                                     { name: `lora_${i}`, type: '*', link_type: LiteGraph.INPUT },
-//                                     { name: `weight_${i}`, type: 'number', default: 1.0 },
-//                                     { name: `model_weight_${i}`, type: 'number', default: 1.0 },
-//                                     { name: `clip_weight_${i}`, type: 'number', default: 1.0 }
-//                                 );
-//                             }
-//                         }
-//                     }
-//                 }
-//                 // Handle mode changes
-//                 else if (name.startsWith("mode_")) {
-//                     const slotNum = name.split("_")[1];
-//                     const isSimple = value === "Simple";
-
-//                     // Remove or add weight inputs based on mode
-//                     if (isSimple) {
-//                         // Remove model and clip weights, add simple weight if not exists
-//                         this.inputs = this.inputs.filter(input =>
-//                             !input.name.match(`(model_weight_${slotNum}|clip_weight_${slotNum})`)
-//                         );
-//                         if (!this.inputs.some(input => input.name === `weight_${slotNum}`)) {
-//                             this.inputs.push({ name: `weight_${slotNum}`, type: 'number', default: 1.0 });
-//                         }
-//                     } else {
-//                         // Remove simple weight, add model and clip weights if not exist
-//                         this.inputs = this.inputs.filter(input =>
-//                             input.name !== `weight_${slotNum}`
-//                         );
-//                         if (!this.inputs.some(input => input.name === `model_weight_${slotNum}`)) {
-//                             this.inputs.push(
-//                                 { name: `model_weight_${slotNum}`, type: 'number', default: 1.0 },
-//                                 { name: `clip_weight_${slotNum}`, type: 'number', default: 1.0 }
-//                             );
-//                         }
-//                     }
-//                 }
-//             };
-//         }
-//     }
-// });
