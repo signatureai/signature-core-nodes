aa05bfdd6f81d84eb4829d9fa7c58a7804c8a37e
Marco João <marco.joao@signature.ai>
2024-12-06 16:01:59 +0000

    feat: add Report class
    Introduced a new Report class in platform_io.py that defines input types and manages execution logic. The class allows for structured reporting with a string output, enhancing the functionality of the PlatformEnvs module.

--- nodes/platform_io.py
+++ nodes/platform_io.py
@@ -569,3 +569,24 @@ class PlatformEnvs:
             ORG_ID,
             ORG_TOKEN,
         )
+
+
+class Report:
+    @classmethod
+    def INPUT_TYPES(cls):  # type: ignore
+        return {
+            "required": {
+                "value": (any_type,),
+                "report": ("STRING", {"default": "", "multiline": True}),
+            },
+        }
+
+    RETURN_TYPES = (any_type,)
+    RETURN_NAMES = ("value",)
+    FUNCTION = "execute"
+    CATEGORY = PLATFORM_IO_CAT
+    OUTPUT_NODE = True
+
+    def execute(self, value, report):
+        data = {"type": "string", "value": report}
+        return {"ui": {"signature_report": [data]}, "result": (value,)}
